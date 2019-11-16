b6ca531f4edcf1f20813c9f14fe4153e233ed837
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-11-16 14:27:01 +0000

    Add incident data

--- server/index.js
+++ server/index.js
@@ -7,61 +7,96 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 app.use(express.static('public'))
 app.listen(port, () => console.log(`Listening on :${port}!`))
 var processedData;
-/*fs.readFile("station_codes.csv", "utf8",function (err, data) {
-    if (err) throw err;
-    processedData = processCsvData(data)
-});*/
-
-fs.readFile("RailReferences.csv", "utf8",function (err, data) {
-    if (err) throw err;
-    processedData = processCsvDataRail(data)
-    //console.log(processedData)
-    fs.writeFile("station_codes.json", JSON.stringify(processedData), "utf8", (err)=>console.error(err))
-
-});
 
 
 const processCsvDataRail = function(data){
 
     var output = []
     var array  = data.split("\n");
-    array.forEach(element => {
-   
+    var a;
+    array.forEach(element => {   
 
-    var a = element.split(',');
-   try{ output.push(
-        {
-            name : a[3].toLowerCase().replace(' ', '').replace(' ', '').replace(' ', '').replace(' ', '').replace('"', '').replace('"', ''),  
-            crs : a[2].toUpperCase().replace('"', '').replace('"', ''),
-            tiploc : a[1].toUpperCase().replace('"', '').replace('"', '')
+        a = element.split(',');
+        let stationName1 = a[8].replace(/ /g, '').trim().toLowerCase()
+        let crs8 = undefined;
+        let tiploc8 = undefined;    
+        for(var i = 0; i < codes.length; ++i){
+            let station1 = codes[i]
+            let str = station1.name
+            stationName1 = stationName1.replace('station', '')
+
+
+            if(stationName1.includes(str) || str.includes(stationName1)){
+                crs8 = station1.crs ;
+                tiploc8 = station1.tiploc ;
+                break;
+            }
+        }  
+
+
+         let stationName = a[9].replace(/ /g, '').trim().toLowerCase()
+        let crs9 = undefined;
+        let tiploc9 = undefined;    
+        for(var i = 0; i < codes.length; ++i){
+            let station = codes[i]
+            let str = station.name
+
+            stationName = stationName.replace('station', '')
+            if(stationName.includes(str) || str.includes(stationName)){
+                crs9 = station.crs ;
+                tiploc9 = station.tiploc ;
+                break;
+            }
         }
-    )}catch{
-        console.log("oi")
-    }})
+
+                
+   try{ 
+       output.push(
+        {
+            date : a[0],
+            incidentNumber : a[1],
+            incidentReason : a[2],
+            incidentReasonDescription: a[3],
+            sectionName : a[4],
+            operatorAffected: a[5],
+            operatorNameAffected : a[6],
+            trainIdAffected: a[7],
+            plannedOriginName: a[8],
+            plannedOriginCRS: crs8,
+            plannedOriginTiploc: tiploc8,
+            plannedDestinationName: a[9],
+            plannedDestinationCRS: crs9,
+            plannedDestinationTiploc: tiploc9,
+            performanceEventCode : a[10],
+            reactionaryCategoryCode : a[11],
+            reactionaryCategoryDescription : a[12],
+            startStanoxDescription: a[13],
+            eventCount : a[13],
+            PfPIMinutes: a[14]
+        });
+    }catch(error){
+        console.error(error)
+    }
+    });
+    //console.log(output)
         return output;
 }
-const processCsvData = function(data){
 
-    var output = []
-    var nameKey = "name";
-    var crsKey = "crs";
+const getStationCRS = function(stationName){
 
-    var array  = data.split("\n");
-    
-    array.forEach(element => {
-        var a = element.split(',');
-        output.push(
-            {
-                name : a[0].toLowerCase().replace(' ', '').replace(' ', '').replace(' ', '').replace(' ', '') ,  
-                crs : crsCode(a[1]),
-                tiploc : "test"
-            }
-        )
-    });
-
-    
-    return output;
 }
+const getStationTiploc = function(stationName){
+    let str = stationName.replace(/ /g, '').trim().toLowerCase()
+        codes.forEach((station)=>{
+            let stationName = station.name.replace('station', '')
+     
+            
+            if(stationName.includes(str) || str.includes(stationName)){
+                return station.tiploc
+            }
+        })    
+    }
+
 
 const crsCode = function(c){
     try {
@@ -72,3 +107,15 @@ const crsCode = function(c){
       }
       
 }
+
+let rawdata = fs.readFileSync('station_codes.json');
+let codes = JSON.parse(rawdata);
+var data = fs.readFileSync("server/15062019.csv", "utf8");
+
+var processedData = processCsvDataRail(data)
+var data1 = fs.readFileSync("server/05072019.csv", "utf8");
+processedData.push(...processCsvDataRail(data1))
+console.log(processedData)
+fs.writeFile("values.json", JSON.stringify(processedData), "utf8", (err)=>console.error(err))
+
+
