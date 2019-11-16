410a864b18fdd8093b4b58de4caa0ce727044a83
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-11-16 12:07:45 +0000

    add tiploc data

--- server/index.js
+++ server/index.js
@@ -6,13 +6,40 @@ const port = 3000
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 app.use(express.static('public'))
 app.listen(port, () => console.log(`Listening on :${port}!`))
-
-fs.readFile("station_codes.csv", "utf8",function (err, data) {
+var processedData;
+/*fs.readFile("station_codes.csv", "utf8",function (err, data) {
     if (err) throw err;
-    const processedData = processCsvData(data)
+    processedData = processCsvData(data)
+});*/
+
+fs.readFile("RailReferences.csv", "utf8",function (err, data) {
+    if (err) throw err;
+    processedData = processCsvDataRail(data)
+    //console.log(processedData)
     fs.writeFile("station_codes.json", JSON.stringify(processedData), "utf8", (err)=>console.error(err))
+
 });
 
+
+const processCsvDataRail = function(data){
+
+    var output = []
+    var array  = data.split("\n");
+    array.forEach(element => {
+   
+
+    var a = element.split(',');
+   try{ output.push(
+        {
+            name : a[3].toLowerCase().replace(' ', '').replace(' ', '').replace(' ', '').replace(' ', '').replace('"', '').replace('"', ''),  
+            crs : a[2].toUpperCase().replace('"', '').replace('"', ''),
+            tiploc : a[1].toUpperCase().replace('"', '').replace('"', '')
+        }
+    )}catch{
+        console.log("oi")
+    }})
+        return output;
+}
 const processCsvData = function(data){
 
     var output = []
@@ -26,7 +53,8 @@ const processCsvData = function(data){
         output.push(
             {
                 name : a[0].toLowerCase().replace(' ', '').replace(' ', '').replace(' ', '').replace(' ', '') ,  
-                crs : crsCode(a[1])
+                crs : crsCode(a[1]),
+                tiploc : "test"
             }
         )
     });
